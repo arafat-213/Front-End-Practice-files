d0fd43c66d7275f08bf6749a37fb1e52cf9c1f54
Arafat Tai <tai.arafat.at@gmail.com>
2017-12-15 20:20:26 +0530

    Initial commit

---
+++ New folder/script.js
@@ -0,0 +1,5 @@
+var fName = prompt("What is your first name?");
+var lName = prompt("WHat is your last name?");
+var age = prompt("What is your age?");
+
+console.log("Nice to meet you, "+fName+" "+lName);
